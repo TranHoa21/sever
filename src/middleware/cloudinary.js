e8db25b8c7a777c5cae74b3dc2bad6b67e832f42
TranHoa21 <soc2k3dangyeu@gmail.com>
2025-05-09 04:56:53 +0700

    up

--- src/middleware/cloudinary.js
+++ src/middleware/cloudinary.js
@@ -1,23 +1,24 @@
 import { v2 as cloudinary } from 'cloudinary';
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const multer = require('multer');
-require('dotenv').config();
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import multer from 'multer';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET
+    api_secret: process.env.CLOUDINARY_SECRET,
 });
 
 const storage = new CloudinaryStorage({
     cloudinary,
     allowedFormats: ['jpg', 'png'],
     params: {
-        folder: 'learn_nodejs'
-    }
+        folder: 'learn_nodejs',
+    },
 });
 
 const uploadCloud = multer({ storage });
 
-module.exports = uploadCloud;
+export default uploadCloud;
